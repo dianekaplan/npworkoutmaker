678abf26a878379690a2778e9fb84186b0954fa0
Diane Kaplan <dianekaplan@gmail.com>
2016-06-11 09:35:29 -0400

    added credits into footer

---
+++ public/credits.php
@@ -0,0 +1,9 @@
+<?php
+
+    // configuration
+    require("../includes/config.php"); 
+    
+    // render the documentation view
+    render("credits.html");
+
+?>
--- views/footer.php
+++ views/footer.php
@@ -3,6 +3,7 @@
             <div id="bottom">
                 Get out there and enjoy! |
                 <a href="disclaimer.php">Disclaimer</a> |
+                <a href="credits.php">Credits</a> |
                 Questions, ideas, suggestions? <a href="mailto:dianekaplan@gmail.com?Subject=Workout%20maker%20feedback">Contact me</a>
             </div>
 
